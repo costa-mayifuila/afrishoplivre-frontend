14f29e4a6a1dd7d633b4440fe1f4b1eeeb711703
Clóvis Costa <businessclovis@gmail.com>
2025-07-19 17:57:08 -0300

    Corrige erro de build externalizando módulos no Vite

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,15 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     tailwindcss(),
-    react()],
+    react()
+  ],
+  build: {
+    rollupOptions: {
+      external: ['firebase-admin', 'fs', 'path', 'pdfkit'] // Adicione todos os possíveis
+    }
+  }
 })
